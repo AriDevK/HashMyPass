a1772073ee4fb9438de7f76979f338e6777ae297
AriDevK <AriadneFave@hotmail.com>
2024-02-17 10:50:47 -0600

    feat(UI): Add mudblazor and dark mode switcher

--- HashMyPass/Program.cs
+++ HashMyPass/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HashMyPass;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
